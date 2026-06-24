624bc4cec1232e6535abc690467198303d7d91a0
Mark Cuizon <foxfacefyynch@gmail.com>
2026-06-24 23:01:41 +0800

    leet

--- Pandas/main.py
+++ Pandas/main.py
@@ -1,8 +1,17 @@
 import numpy as np
 import pandas as pd
 
-data = [100, 102, 104]
+data = {
+    "Name": ["John", "Jane", "Jim", "Jill"],
+    "Age": [25, 30, 35, 40],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston"],
+}
 
-series = pd.Series(data, index=["a", "b", "c"])
+dataframe = pd.DataFrame(data, index=["a", "b", "c", "d"])
 
-print(series)
+dataframe["Position"] = ["Manager", "Developer", "Designer", "Developer"]
+
+new_row = pd.DataFrame([{"Name": "Sandy", "Age": 28, "City": "Pasig", "Position": "Intern"}])
+dataframe = pd.concat([dataframe, new_row])
+
+print(dataframe) 
